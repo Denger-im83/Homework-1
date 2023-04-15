ef2871887ab29a44424bee083765f524b0fb9ce9
Denis <denis_im@mail.ru>
2023-04-15 20:08:28 +0500

    максимум трех чисел

---
+++ Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("введите число a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("введите число b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("введите число c: ");
+double c = double.Parse(Console.ReadLine());
+
+Console.Write(" Максимальное число из трех:" + Math.Max(a, Math.Max(b,c)));
