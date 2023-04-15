de7bd4f10d04232a7b766ce823d45f75967dbc77
Denis <denis_im@mail.ru>
2023-04-15 20:05:48 +0500

    максимум двух чисел

---
+++ Task 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("введите число a: ");
+
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("введите число b: ");
+
+double b = double.Parse(Console.ReadLine());
+Console.Write(" Максимальное число из двух:" + Math.Max(a,b));
+
+Console.Write(" Минимальное число из двух:" + Math.Min(a,b));
