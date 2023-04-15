ec8ca1fc2263354beb5e1a0913ac2cb74a3d0288
Denis <denis_im@mail.ru>
2023-04-15 20:30:59 +0500

    проверка четное число или нет

---
+++ Task 3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+double b = a % 2;
+switch (b)
+{
+    case 0: System.Console.WriteLine("Число a четное"); break;
+    default: System.Console.WriteLine("Число a нечетное"); break;
+}
