47b004ec0d94ed70584561e82b5fa5be8e5ca11c
Denis <denis_im@mail.ru>
2023-04-15 20:40:06 +0500

    поиск четных чисел в диапазоне

---
+++ Task 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+for (int b = 2; b <= a; b += 2)
+ {if (b < a - 1)
+        Console.Write(b + ", ");
+        else
+        Console.Write(b + ".");};
+        
